8e308a6f913ddec7753cf81157dd39937c18350c
Germán <german.hohmann@gmail.com>
2018-10-16 15:07:47 -0300

    Broadcast messages when users connect/disconnects

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  
+  socket.broadcast.emit('a user connected');
+
   socket.on('chat message', (msg) => {
     console.log(`message: ${msg}`);
     io.emit('chat message', msg);
   });
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('a user disconnected');
+    socket.broadcast.emit('a user disconnected');
   });
 });
 
